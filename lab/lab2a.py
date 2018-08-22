7e096465c8b5b477e5bd4b4bdbabb1c3323f0fd7
parvezmrobin@gmail.com <qwer56uiop>
2018-08-22 11:40:19 +0600

    just show the images

--- lab/lab2a.py
+++ lab/lab2a.py
@@ -1,6 +1,6 @@
 import os
 from tkinter import *
-from tkinter.filedialog import askdirectory, asksaveasfile, askopenfile
+from tkinter.filedialog import askdirectory, asksaveasfile, askopenfilename
 
 import numpy as np
 from matplotlib.pyplot import imread
@@ -12,12 +12,14 @@ true, false, null = True, False, None
 class Similarity:
 
     def __init__(self):
-        self.root = Tk(className="Similarity")
+        self.root = Tk()
         self.root.title("Data Mining")
         self.status_text = StringVar()
+        self.canvas = null
 
         self.train_dir = null
         self.train_data = null
+        self.file_names = null
 
         self.means = []
         self.medians = []
@@ -28,14 +30,18 @@ class Similarity:
         self.std_dev = []
         self.df = null
 
+        self.query_image = null
+        self.feature_vector = null
+        self.nearest_images = null
+
         self.create_gui()
 
     def create_gui(self):
         config = {"padx": 10, "pady": 10, "text": "Output Area"}
         output_frame = LabelFrame(self.root, **config)
 
-        canvas = Canvas(output_frame)
-        canvas.pack()
+        self.canvas = Canvas(output_frame)
+        self.canvas.pack()
         output_frame.pack()
 
         config["text"] = "Options"
@@ -56,11 +62,11 @@ class Similarity:
         btn_load_features.pack(side=LEFT)
 
         config["text"] = "Select Query Image"
-        btn_select_img = Button(btn_frame, **config)
+        btn_select_img = Button(btn_frame, **config, command=self.load_query_image)
         btn_select_img.pack(side=LEFT)
 
         config["text"] = "Show Similar Images"
-        btn_show_img = Button(btn_frame, **config)
+        btn_show_img = Button(btn_frame, **config, command=self.show_similar_images)
         btn_show_img.pack(side=LEFT)
 
         status_label = Label(self.root, pady=5, textvariable=self.status_text)
@@ -79,8 +85,8 @@ class Similarity:
 
         file_names = os.listdir(self.train_dir)
         n = len(file_names)
-        file_names = [self.train_dir + os.sep + file_name for file_name in file_names]
-        images = [imread(file_name) for file_name in file_names]
+        self.file_names = [self.train_dir + os.sep + file_name for file_name in file_names]
+        images = [imread(file_name) for file_name in self.file_names]
         images = np.array(images)
         gray_images = np.mean(images, axis=3)
         self.train_data = np.reshape(gray_images, (n, -1))
@@ -112,27 +118,16 @@ class Similarity:
         assert self.means.shape == self.medians.shape == self.modes.shape == self.mid_ranges.shape
         assert self.mid_ranges.shape == self.ranges.shape == self.iqrs.shape == self.std_dev.shape == (m,)
 
-        data = np.array([self.means, self.medians, self.modes,
-                         self.mid_ranges, self.ranges, self.iqrs, self.std_dev])
-        columns = ["Mean", "Median", "Mode", "MidRange", "Range", "IQR", "StdDev"]
+        data = np.array([self.means, self.medians, self.modes, self.mid_ranges,
+                         self.ranges, self.iqrs, self.std_dev, self.file_names])
+        columns = ["Mean", "Median", "Mode", "MidRange", "Range", "IQR", "StdDev", "FileName"]
         self.df = DataFrame(data.T, columns=columns)
 
         self.status_text.set("Features extracted")
 
         self.save_extracted_features()
 
-    def load_features(self):
-        filetypes = (("CSV File", "*.csv"), ("All Files", "*.*"))
-        feature_file = askopenfile(filetypes=filetypes)
-        if feature_file == '':
-            self.status_text.set("No file selected.")
-            return
-        self.df = read_csv(feature_file)
-        self.status_text.set('Successfully read features from "{}".'.format(feature_file.name))
-        feature_file.close()
-
     def save_extracted_features(self):
-        self.df: DataFrame
         if self.df is null:
             return
         filetypes = (("CSV File", "*.csv"), ("All Files", "*.*"))
@@ -146,20 +141,83 @@ class Similarity:
             self.df.to_csv(handle)
             self.status_text.set('Features are written to "' + handle.name + '"')
         except PermissionError as err:
-            self.status_text.set('Could not open file "' + err.filename + '"')
+            self.status_text.set('Could not write to file "' + err.filename + '"')
         finally:
             if handle is not null:
                 handle.close()
 
+    def load_features(self):
+        filetypes = (("CSV File", "*.csv"), ("All Files", "*.*"))
+        feature_file = askopenfilename(filetypes=filetypes)
+        if feature_file == '':
+            self.status_text.set("No file selected.")
+            return
+        self.df = read_csv(feature_file, index_col=0)
+        self.status_text.set('Successfully read features from "{}".'.format(feature_file))
+
+    def load_query_image(self):
+        image_file = askopenfilename()
+        if image_file == '':
+            self.status_text.set("No image selected.")
+            return
+        self.query_image = imread(image_file)
+        self.query_image = np.mean(self.query_image, axis=2).reshape((-1))
+        self.status_text.set('Successfully read query image : "{}"'.format(image_file))
+
+    def show_similar_images(self):
+        if self.df is null:
+            self.status_text.set("Load the features first.")
+            return
+        if self.query_image is null:
+            self.status_text.set("Load the query image first.")
+            return
+
+        self.feature_vector = [
+            np.mean(self.query_image),
+            np.median(self.query_image),
+            Similarity._mode1d(self.query_image)
+        ]
+        max_val = np.max(self.query_image)
+        min_val = np.min(self.query_image)
+        self.feature_vector += [(max_val + min_val) * .5, (max_val - min_val)]
+
+        sorted_values = np.sort(self.query_image)
+        q_size = int(sorted_values.shape[0] / 4)
+        q1 = sorted_values[q_size]
+        q3 = sorted_values[q_size * 3]
+        self.feature_vector.append(q3 - q1)
+
+        self.feature_vector.append(np.std(self.query_image))
+        assert len(self.feature_vector) == len(self.df.columns) - 1
+
+        self.find_nearest_images()
+
+    def find_nearest_images(self, pick=3):
+        def distance(df: DataFrame):
+            cols = ['Mean', 'Median', 'Mode', 'MidRange', 'Range', 'IQR', 'StdDev']
+            df = df[cols]
+            for i, col in enumerate(cols):
+                df[col] = (df[col] - self.feature_vector[i]) ** 2
+            diff_col = df.sum(axis=1)
+            return diff_col
+
+        self.df: DataFrame
+        temp_df = self.df.assign(Diff=distance).sort_values('Diff')
+        self.nearest_images = temp_df.FileName[:pick]
+
+        self.canvas: Canvas
+        img = PhotoImage(file=self.nearest_images.iloc[1])
+        self.canvas.create_image(50, 50, anchor=NE, image=img)
+
+    @staticmethod
+    def _mode1d(arr):
+        u, c = np.unique(arr, return_counts=true)
+        max_index = np.argmax(c)
+        return u[max_index]
+
     @staticmethod
     def _modes(train_data):
-        def mode1d(arr):
-            u, c = np.unique(arr, return_counts=true)
-            max_index = np.argmax(c)
-            return u[max_index]
-
-        modes = np.apply_along_axis(mode1d, 1, train_data)
-
+        modes = np.apply_along_axis(Similarity._mode1d, 1, train_data)
         return modes
 
 
